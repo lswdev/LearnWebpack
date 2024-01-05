1d440775fd5fe848c96472c73098a929d9307eb1
lswsepia <lswsepia@gmail.com>
2024-01-05 15:01:17 +0900

    lodash를 불러오고 사용하는 과정

--- getting-started/src/index.js
+++ getting-started/src/index.js
@@ -1,11 +1,11 @@
-import _ from 'lodash';
+import _ from 'lodash';   // package.json 에 설치 완료된 상태여야 한다.
 
 function component() {
   var element = document.createElement('div');
 
   /* lodash is required for the next line to work */
-  element.innerHTML = _.join(['hello','webpack'], ' ');
-
+  element.innerHTML = _.join(['hello','webpack'], ' ');   // locash  _를 통해 접근 가능
+  
   return element;
 }
 
