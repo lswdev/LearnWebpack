b1a557189e75e59e79abec2d4544f75faa46048f
lswsepia <lswsepia@gmail.com>
2024-01-08 14:59:23 +0900

    webpack mode / entry / output / Loader 부가 설명

--- code-splitting2/webpack.config.js
+++ code-splitting2/webpack.config.js
@@ -1,13 +1,13 @@
 var path = require('path');
 
 module.exports = {
-  mode: 'none',
-  entry: './index.js',
+  mode: 'none',   // production(베포할 때에는 production 모드로 변환해야한다.), development(개발), none(교육목적)
+  entry: './index.js',    // 대상 파일의 경로, 주소
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.js',    // [name], [id], [chunkhash].js 등등 다양한 형태가 있다.
     path: path.resolve(__dirname, 'dist')
   },
-  module: {
+  module: {   // Loader
     rules: [
       {
         test: /\.css$/,
