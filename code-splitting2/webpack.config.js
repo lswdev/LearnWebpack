962081ed8549e1096da3b7240363f5bfba180daa
lswsepia <lswsepia@gmail.com>
2024-01-08 16:30:11 +0900

    Plugin을 사용하여 css를 분리했다. index.html에서 따로 link 태그로 연결

--- code-splitting2/webpack.config.js
+++ code-splitting2/webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: 'none',   // production(베포할 때에는 production 모드로 변환해야한다.), development(개발), none(교육목적)
@@ -11,13 +12,21 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        // use: ['style-loader', 'css-loader']
         // css-loader는 css가 webpack 안에 들어갈 수 있게 하는 역할이고,
         // style-loader는 css 코드를 <head> 태그 안쪽에 inline으로 들어갈 수 있게 해주는 역할이다.
         // 따라서 css-loader가 먼저 실행이 된 후에 style-loader가 두번째로 실행되어야 한다.
         // build 시 우측부터 우선적으로 실행되므로 위 순서대로 작성해야 한다.
         // 만일 sass를 사용하는 환경이라면 ['style-loader', 'css-loader', 'sass-loader'] 순서가 될 것이다.
+
+        use: [,
+          { loader : MiniCssExtractPlugin.loader },
+          "css-loader"
+        ]
       }
     ]
   },
+  plugins: [
+      new MiniCssExtractPlugin()
+  ]
 }
